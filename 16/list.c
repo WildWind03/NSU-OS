7d247f4b8ecc93dd07c4754742cf76bb977eb32c
WildWind03 <usergreat03@gmail.com>
2016-10-10 18:15:57 +0700

    List is almost ready

--- 16/list.c
+++ 16/list.c
@@ -1,11 +1,11 @@
-#define MAX_LENGTH_OF_STRING 80
-#define ALLOCATE_ERROR -1
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#define MAX_LENGTH_OF_STRING 80
+#define ALLOCATE_ERROR -1
 
 typedef struct node {
   node *next;
@@ -14,35 +14,87 @@ typedef struct node {
 
 typedef struct list {
   node *lastNode;
-  pthread_mutex_t *mutex;
+  pthread_mutex_t mutex;
   size_t size;
 } List;
 
 int initList(List *list) {
+  list = (List*) malloc (sizeof(List));
   list -> size = 0;
-  return pthread_mutex_init(list -> mutex, NULL);
+  return pthread_mutex_init(&list -> mutex, NULL);
 }
 
 int destroyList(List *list) {
-  return pthread_mutex_destroy(list -> mutex);
+  for (int k = 0; k < size; ++k) {
+
+  }
+  
+  return pthread_mutex_destroy(&list -> mutex);
 }
 
-int add(List *list, char *text) {
-  pthread_mutex_lock(list -> mutex);
-
+int add_(List *list, char *text, int start, int end){
   Node *node = (Node*) malloc (sizeof(Node));
   if (NULL == node) {
     return ALLOCATE_ERROR;
   }
 
   int length = strlen(text);
-  node -> text = (char*) malloc (sizeof(char) * length);
-  strcpy(node -> text, text);
+  if (end > length) {
+    end = length;
+  }
+
+  node -> text = (char*) malloc (sizeof(char) * (end - start));
+  strncpy(node -> text, text + start, end - start);
 
   node -> next = list -> lastNode;
   list -> lastNode = node;
 
-  pthread_mutex_unlock(list -> mutex);
-
   return 0;
+}
+
+int add(List *list, char *text) {
+  pthread_mutex_lock(&list -> mutex);
+
+  int length = strlen(text);
+  int result = 0;
+
+  for (int k = 0; k < length; k+=MAX_LENGTH_OF_STRING) {
+    result = add_(list, text, k, k + MAX_LENGTH_OF_STRING);
+    if (result < 0) {
+      break;
+    }
+  }
+
+  pthread_mutex_unlock(&list -> mutex);
+
+  return result;
+}
+
+int bubbleSort(List *list) {
+  pthread_mutex_lock(&list -> mutex);
+
+  for (int k = 0; k < list -> size - 1; ++k) {
+    bool swapped = false;
+    Node *leftNode = list -> lastNode;
+
+    for (int i = 0; i < list -> size - 1 - k; ++i) {
+      Node *rightNode = leftNode -> next;
+
+      if ((leftNode -> text)[0] > (rightNode -> text)[0]) {
+        char *bufferText = rightNode -> text;
+        rightNode -> text = leftNode -> text;
+        leftNode -> text = bufferText;
+
+        swapped = true;
+      }
+
+      leftNode = rightNode;
+    }
+
+    if (!swapped) {
+      break;
+    }
+  }
+
+  pthread_mutex_unlock(&list -> mutex);
 }
--- 16/main.c
+++ 16/main.c
@@ -1,5 +1,8 @@
 #include "list.c"
 
 int main(int argc, char const *argv[]) {
+  List list;
+  initList(&list);
+  destroyList(&list);
   return 0;
 }
