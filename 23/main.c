fc2f4a61d04341e37c33ae7e9917c82ec1c018c8
WildWind03 <usergreat03@gmail.com>
2016-10-24 22:41:21 +0700

    Ready to release

--- 23/main.c
+++ 23/main.c
@@ -7,27 +7,53 @@
 #include <string.h>
 #include <semaphore.h>
 
+queue myqueue;
+
+void* work1(void *arg) {
+    for (int k = 0; k < 10; ++k) {
+        mymsgput(&myqueue, "Tread 1 wrote");
+        usleep(1000000);
+    }
+}
+
+void* work2(void *arg) {
+  for (int k = 0; k < 10; ++k) {
+      mymsgput(&myqueue, "Tread 2 wrote");
+      usleep(2000000);
+  }
+}
+
+void* work3(void *arg) {
+  for (int k = 0; k < 10; ++k) {
+    char buffer[20];
+    mymsgget(&myqueue, buffer, 20);
+    printf ("'%s' was written by thread 3\n", buffer);
+    usleep(500000);
+  }
+}
+
+void* work4(void *arg) {
+  for (int k = 0; k < 10; ++k) {
+    char buffer[20];
+    mymsgget(&myqueue, buffer, 20);
+    printf ("'%s' was written by thread 4\n", buffer);
+    usleep(800000);
+  }
+}
+
 int main() {
-  queue queue;
-  mymsginit(&queue);
-  mymsgput(&queue, "Hello");
-  mymsgput(&queue, "Hello1");
-  mymsgput(&queue, "Hello2");
-  mymsgput(&queue, "Hello3");
+  mymsginit(&myqueue);
+  pthread_t pthread1, pthread2, pthread3, pthread4;
 
-  char buffer[20];
+  pthread_create (&pthread1, NULL, work1, NULL);
+  pthread_create (&pthread2, NULL, work2, NULL);
+  pthread_create (&pthread3, NULL, work3, NULL);
+  pthread_create (&pthread4, NULL, work4, NULL);
 
-  mymsgget(&queue, buffer, 20);
-  printf ("%s\n", buffer);
-
-  mymsgget(&queue, buffer, 20);
-  printf ("%s\n", buffer);
-
-  mymsgget(&queue, buffer, 20);
-  printf ("%s\n", buffer);
-
-  mymsgget(&queue, buffer, 20);
-  printf ("%s\n", buffer);
-  mymsgdestroy(&queue);
+  pthread_join(pthread1, NULL);
+  pthread_join(pthread2, NULL);
+  pthread_join(pthread3, NULL);
+  pthread_join(pthread4, NULL);
+  mymsgdestroy(&myqueue);
   return 0;
 }
